24878b3a25b71136a882581d249da4978dce7365
OS0day <os0day@qq.com>
2022-08-08 00:46:51 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: './'
+    plugins: [vue()],
+    base:'/myhome/'   // 对应GitHub项目名称
 })
